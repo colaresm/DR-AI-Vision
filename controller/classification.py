6c61b5a2b99927ababecbf8b6686fb7324a3230b
Marcelo Colares <marcelocolares@MacBook-Air-de-Marcelo.local>
2025-10-27 17:14:22 -0300

    Create frontend

--- controller/classification.py
+++ controller/classification.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, jsonify, request
 from use_cases.predict import predict
 user_bp = Blueprint('user', __name__)
-
+import time
 @user_bp.route('/', methods=['GET'])
 def listar_usuarios():
+   start = time.time()
    print(predict("test/no_dr.png"))
+   end = time.time()
+   print("time",end-start)
    return jsonify(predict("test/no_dr.png"))
